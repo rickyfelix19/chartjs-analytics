a88e722990ebed243fd1431709354a99ea2cde5c
Ricky Felix <rickyfelix19@outlook.com>
2021-09-13 12:32:04 +1000

    make graph componnets

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Graphs from './components/Graphs/Graphs';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <u style={{ color: 'lightblue' }}>Learn React</u>
+        </p>
       </header>
+      <div>
+        <Graphs />
+      </div>
     </div>
   );
 }
